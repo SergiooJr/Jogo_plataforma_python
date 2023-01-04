bb7717e513adfba097316e116c04b1a11ac85c54
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2023-01-04 19:35:50 -0300

    Update video_12_sprites.py

--- video_12_sprites.py
+++ video_12_sprites.py
@@ -12,24 +12,31 @@ pygame.display.set_caption('Sprites')
 
 preto = (0, 0, 0)
 
-class Sapo(pygame.sprite.Sprite): # essa classe sapo, vai herdar de uma outra classe do pygame, chamada Sprite
+# Criando a classe Sapo
+class Sapo(pygame.sprite.Sprite): # essa classe Sapo, vai herdar de uma outra classe do pygame, chamada Sprite
     def __init__(self):
         pygame.sprite.Sprite.__init__(self) # obrigatório
-        self.sprites = []
+        self.sprites = [] # variável (atributo) que vai armazenar todas as sprites em uma lista
+        # armazenando as sprites
         for i in range(1, 11):
-            self.sprites.append(pygame.image.load(f'sprites/attack_{i}.png'))
+            self.sprites.append(pygame.image.load(f'sprites/attack_{i}.png')) # adicionando e carregando as imagens
+            # dentro da lista
         self.atual = 0
-        self.image = self.sprites[self.atual]
+        self.image = self.sprites[self.atual] # recebe o 1° elemento da nossa lista sprites
 
+        # definindo o tamanho da imagem
         self.image = pygame.transform.scale(self.image, (128*3, 64*3))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 100, 100
 
+        # definindo a posição em que a imagem vai aparecer na tela
+        self.rect = self.image.get_rect() # pegando o retângulo que fica ao redor da imagem
+        self.rect.topleft = 100, 100 # colocando o canto superior esquerdo da imagem a 100px em X, e 100px em Y
+
+# .......................................................ANIMANDO................................................
         self.animar = False
-
     def atacar(self):
         self.animar = True
 
+    # método herdado da classe sprite
     def update(self):
         if self.animar:
             self.atual += 0.5
@@ -40,6 +47,7 @@ class Sapo(pygame.sprite.Sprite): # essa classe sapo, vai herdar de uma outra cl
             self.image = pygame.transform.scale(self.image, (128 * 3, 64 * 3))
 
 todas_as_sprites = pygame.sprite.Group()
+# instanciando o objeto
 sapo = Sapo()
 todas_as_sprites.add(sapo)
 
